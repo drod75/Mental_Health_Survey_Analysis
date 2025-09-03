20497a008639c7c3a5ded76902b1df735fc6bd1e
David Rodriguez <dr507498@gmail.com>
2025-09-03 12:04:38 -0400

    Repository Setup Completed

---
+++ src/util/supabase-script.py
@@ -0,0 +1,73 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class SupabaseScript:
+    def __init__(self) -> None:
+        load_dotenv()
+
+        self.supabase_url = os.getenv("SUPABASE_URL")
+        self.supabase_key = os.getenv("SUPABASE_KEY")
+
+        self.supabase: Client = create_client(self.supabase_url, self.supabase_key)  # type: ignore
+
+    def fetch_data(self, table_name: str, **filters: str):
+        """
+        Fetches data from a Supabase table with multiple filter conditions.
+
+        Args:
+            table_name: The name of the table to fetch data from.
+            **filters: Keyword arguments where the key is the column name
+                       and the value is the filter value.
+
+        Returns:
+            The data from the table that matches the filter conditions.
+        """
+        try:
+            query = self.supabase.table(table_name).select("*")
+
+            for column, value in filters.items():
+                query = query.eq(column, value)
+
+            response = query.execute()
+
+            if response.data:
+                logger.info(f"Successfully fetched data from {table_name}")
+                return response.data
+            else:
+                logger.warning(f"No data found for the given filters in {table_name}.")
+                return None
+
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+
+    def inset_data(self, table_name: str, data: dict) -> bool:
+        """_summary_
+
+        Args:
+            table_name (str): _description_
+            data (dict): _description_
+
+        Returns:
+            bool: Inidcator whether or not operation succeeded
+        """
+        try:
+            query = self.supabase.table(table_name).insert(data)
+            response = query.execute()
+
+            if response.data:
+                logger.info(f"Successfully inserted data into {table_name}")
+            else:
+                logger.warning(f"Failed to insert data into {table_name}.")
+                return False
+
+        except Exception as E:
+            logger.error(f"An error occurred: {E}")
+            return False
+
+        return True
